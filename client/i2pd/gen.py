ffef32a83281d120587993165fdff4b6288686f6
Your Name <you@example.com>
2025-12-27 14:28:56 +0300

    don't answer on ping requests, set ack delay 20ms, make tunnels keys persistant

--- client/i2pd/gen.py
+++ client/i2pd/gen.py
@@ -5,17 +5,18 @@ address=127.0.0.1
 port={1}
 destination=[CENSORED].b32.i2p
 destinationport=1337
-keys=transient-o{0}.dat
+keys=o{0}.dat # transient-
 inbound.quantity=8
 outbound.quantity=8
 inbound.length=4
 outbound.length=4
 inbound.lengthVariance=-1
 outbound.lengthVariance=-1
-i2p.streaming.initialAckDelay=200
 gzip=true
+i2p.streaming.initialAckDelay=20
 i2p.streaming.profile=2
 i2p.streaming.maxWindowSize=768
+i2p.streaming.answerPings=false
 crypto.ratchet.inboundTags=2400
 """.format(index, 3000+index, index)
 
